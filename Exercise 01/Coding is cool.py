cd0ab98f2a1529b13095925ed1c587f6533b8ef9
Hudaahh <lifeofhuda18@gmail.com>
2024-10-16 14:43:37 +0400

    Create Coding is cool.py

---
+++ Exercise 01/Coding is cool.py
@@ -0,0 +1,7 @@
+#Fill in the blanks in the Python code below to output the phrase “Coding is Cool” to the console using variables and string concatenation.
+
+word1= "coding "
+word2= "is "
+word3= "cool "
+result= word1 + " " + word2 + " " + word3
+print (result)
