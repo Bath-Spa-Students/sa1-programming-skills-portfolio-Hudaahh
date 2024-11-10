959c4fb82b018bd83b4bd7d6d75035bf3ceb53e1
Hudaahh <lifeofhuda18@gmail.com>
2024-11-10 23:58:59 +0400

    PrimitiveQuizadvance.py

---
+++ Exercise 04/PrimitiveQuizstandard.py
@@ -0,0 +1,9 @@
+# Exercise no 04, Primitive Quiz standard
+# Write the Capital of france
+Cap =  input("wht is the capital of france: ")
+#The answer should be paris
+if Cap == "Paris": 
+    print("The answer is correct")
+#If its not paris the output shows incorrect 
+else:
+    print("The answer is incorrect")
