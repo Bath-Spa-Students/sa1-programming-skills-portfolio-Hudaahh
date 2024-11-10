ce6a63814d5970b76801b6b5316ec49e3af26b99
Hudaahh <lifeofhuda18@gmail.com>
2024-11-11 01:43:06 +0400

    Hello.py

---
+++ Exercise 09/Hello.py
@@ -0,0 +1,8 @@
+def hello():
+    print("Hello")  # This line outputs "Hello" to the console
+
+def main():
+    hello()  # The line contacts the hello() function
+
+if __name__ == "__main__":
+    main()  # This appeals the main() function when the script is run
