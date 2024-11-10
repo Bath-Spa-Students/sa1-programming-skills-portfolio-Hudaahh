bb42dcdbd58c780fd88e6ef254d6cb57679061f2
Hudaahh <lifeofhuda18@gmail.com>
2024-11-11 00:00:25 +0400

    SomeCounting.py

---
+++ Exercise 07/SomeCounting.py
@@ -0,0 +1,24 @@
+# Loop that counts up from 0 to 50 increases by 1
+print("Counting up from 0 to 50:")
+for i in range(0, 51):
+    print(i)
+
+# Loop that counts down from 50 to 0 by decreases of 1
+print("\nCounting down from 50 to 0:")
+for i in range(50, -1, -1):
+    print(i)
+
+# Loop that counts up from 30 to 50 by additions of 1
+print("\nCounting up from 30 to 50:")
+for i in range(30, 51):
+    print(i)
+
+# Loop that counts down from 50 to 10 by reductions of 2
+print("\nCounting down from 50 to 10 in reductions of 2:")
+for i in range(50, 9, -2):
+    print(i)
+
+# Loop that counts up from 100 to 200 in increments of 5
+print("\nCounting up from 100 to 200 in increments of 5:")
+for i in range(100, 201, 5):
+    print(i)
