3e93806775b4ab0dcc079c6140b9d6e361c3eafe
Hudaahh <lifeofhuda18@gmail.com>
2024-11-10 23:52:45 +0400

    SimpleSums.py

---
+++ Exercise 02/SimpleSums.py
@@ -0,0 +1,19 @@
+# Exercise no 2.
+# Declare a variable and initialize it with the integer value 8.
+Num = 8 
+# Declare a second variable and initialize it with the integer value 10.
+Num2 = 10
+# Declare a third variable that stores the sum of first two numbers.
+Sum = Num + Num2
+# Print the value of the sum to the console.
+print ("The total Sum is : ",Sum)
+
+
+
+
+
+
+
+
+
+
