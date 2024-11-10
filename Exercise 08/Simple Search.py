3fb90913f48a67bfc0339afff8b51cb2a2d0d7b3
Hudaahh <lifeofhuda18@gmail.com>
2024-11-10 22:50:16 +0400

    Simple Search.py

---
+++ Exercise 08/Simple Search.py
@@ -0,0 +1,11 @@
+# Assign the list with specific names
+names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
+
+# Illustrate the target name to search for
+target_name = "Sam"
+
+# Find  the target name in the list
+if target_name in names:
+    print(f"{target_name} found in the list!")
+else:
+    print(f"{target_name} not found in the list.")
