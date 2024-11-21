7d2602fe4e119f34f3ff5ee58bd4e7777be06eb9
Hudaahh <lifeofhuda18@gmail.com>
2024-11-21 21:40:43 +0400

    List-Names.py

---
+++ z_bonus_exercise/List-Names.py
@@ -0,0 +1,6 @@
+# assigning names in a list
+names = ["Anushara", "Haifa", "Rifa", "Shazin", "Sinan", "Amaan"]
+
+# displaying each name by accessing each element in the list
+for name in names:
+    print(name)
