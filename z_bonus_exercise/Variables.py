913c8b84e02c0fb4ea0c27454c08f2231dbf1609
Hudaahh <lifeofhuda18@gmail.com>
2024-11-21 21:36:53 +0400

    Variables.py

---
+++ z_bonus_exercise/Variables.py
@@ -0,0 +1,7 @@
+# Provide a message to a variable
+message = "Heyyy, this is the first message as text."
+print(message)
+
+# Modify and change the value of the variable to a new message
+message = "Now, this is the new updated message as text."
+print(message)
