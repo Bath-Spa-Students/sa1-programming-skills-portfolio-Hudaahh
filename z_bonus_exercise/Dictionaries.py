26358339235f04a7d3c65331c8b7046f380c4561
Hudaahh <lifeofhuda18@gmail.com>
2024-11-21 21:45:47 +0400

    Dictionaries.py

---
+++ z_bonus_exercise/Dictionaries.py
@@ -0,0 +1,13 @@
+# Making  the dictionary
+person = {
+    "first_name": "Huda",
+    "last_name": "Thaha",
+    "age": 25,  # sample age
+    "city": "Dubai"
+}
+
+# displaying each piece of information
+print(f"First Name: {person['first_name']}")
+print(f"Last Name: {person['last_name']}")
+print(f"Age: {person['age']}")
+print(f"City: {person['city']}")
