869a26632fefd356c359c9b05f46c83af667327d
Hudaahh <lifeofhuda18@gmail.com>
2024-11-21 10:05:27 +0400

    Daysofthemonthadvanced.py

---
+++ Exercise 05/Daysofthemonthstandard.py
@@ -0,0 +1,34 @@
+# Developing a dictionary, assosciating each month with its total number of days.
+Number_of_days = {
+    # January 
+    1:31,
+    # February 
+    2:28,
+    # March
+    3:31,
+    # April
+    4:30,
+    # May
+    5:31,
+    # June
+    6:30,
+    # July
+    7:31,
+    # August 
+    8:31,
+    # September
+    9:30,
+    # October
+    10:31,
+    # November
+    11:30,  
+    # December
+    12:31,
+}
+# Seeking the id to enter the month number.
+month_number= int(input("Type the month number: "))
+# Validating the month and printing the numbers of days.
+if 1 <= month_number<= 12:
+    print(f"The number of days in month {month_number} is {Number_of_days[month_number]}")
+else:
+    print("Invalid month number!")
