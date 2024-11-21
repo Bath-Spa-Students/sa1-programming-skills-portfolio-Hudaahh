62ff2706c1c87eb2c184314fce35b54bff036131
Hudaahh <lifeofhuda18@gmail.com>
2024-11-21 21:49:39 +0400

    Functions -Favorite Book.py

---
+++ z_bonus_exercise/Functions -Favorite Book.py
@@ -0,0 +1,6 @@
+def favorite_book(title):
+    """Print a message about one of your favorite books."""
+    print(f"One of my favorite books is {title}.")
+
+# Naming the function with a book title given as an argument
+favorite_book("It Ends With Us")
