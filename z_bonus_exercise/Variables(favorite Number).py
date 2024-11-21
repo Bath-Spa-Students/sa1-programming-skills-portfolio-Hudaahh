cb2eaf077912a37497f04a7f83723b15f487aa0f
Hudaahh <lifeofhuda18@gmail.com>
2024-11-21 21:38:36 +0400

    Variables(favorite Number).py

---
+++ z_bonus_exercise/Variables(favorite Number).py
@@ -0,0 +1,3 @@
+favorite_number = 17  # Using a variable to illustrate the favorite digits
+message = f"My favorite number is {favorite_number}."  # Making a message
+print(message)  # Displaying the message
