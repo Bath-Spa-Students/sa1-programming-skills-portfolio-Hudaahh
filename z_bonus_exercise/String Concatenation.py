e9c21e723cb82064251940001f3bcd633feb3c36
Hudaahh <lifeofhuda18@gmail.com>
2024-11-21 21:35:09 +0400

    String Concatenation.py

---
+++ z_bonus_exercise/String Concatenation.py
@@ -0,0 +1,10 @@
+# elaborate the strings
+string1 = "Hello"
+string2 = "World"
+string3 = "!"
+
+# add the strings into one
+combined_string = string1 + " " + string2 + string3
+
+# Print the added string
+print(combined_string)
